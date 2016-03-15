e91a3db70e5850cb6fee1974c37bfa7158f67b67
Patrick Fournier <patrick@whiskyechobravo.com>
2016-03-14 22:03:09 -0400

    Add argument handling.

--- kleverklog.py
+++ kleverklog.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
+import argparse
 import datetime
 import json
+import os
 import re
 import select
 import sys
@@ -14,10 +16,6 @@ from colored import fg, bg, attr
 
 from kafka import KafkaConsumer
 
-topic = 'all.json'
-servers = ['localhost:9092']
-replay = False
-pattern = None
 colors = {'debug': attr(0),
           'info': fg('cyan'),
           'warn': fg('orange_1'),
@@ -29,14 +27,25 @@ msg_format = "{level} {time}: {msg} [{topic}:{offset}]"
 
 
 if __name__ == "__main__":
-    if replay:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("topic")
+    parser.add_argument("-H", "--host", type=str, help="Kafka server and port.",
+                        default="localhost:9092")
+    parser.add_argument("-r", "--replay", action="store_true",
+                        help="Display all available log entries.", default=False)
+    parser.add_argument("-m", "--match", type=str, help="Initial match pattern.", default=None)
+    args = parser.parse_args()
+
+    pattern = args.match
+
+    if args.replay:
         auto_offset_reset = 'earliest'
     else:
         auto_offset_reset = 'latest'
 
-    consumer = KafkaConsumer(topic,
+    consumer = KafkaConsumer(args.topic,
                              group_id=None,
-                             bootstrap_servers=servers,
+                             bootstrap_servers=args.host,
                              value_deserializer=lambda m: json.loads(m.decode('ascii')),
                              auto_offset_reset=auto_offset_reset)
 
@@ -68,9 +77,9 @@ if __name__ == "__main__":
         po.register(sys.stdin, select.POLLIN)
         if po.poll(0):
             ch = sys.stdin.read(1)
-
             if ch == 'm':
-                pattern = sys.stdin.readline().rstrip('\n').encode('utf-8')
+                pattern = raw_input('Enter match pattern:')
+                pattern = pattern.rstrip('\n').encode('utf-8')
             elif ch == 'r':
                 offset = sys.stdin.readline().rstrip('\n').encode('utf-8')
                 offset = int(offset)
